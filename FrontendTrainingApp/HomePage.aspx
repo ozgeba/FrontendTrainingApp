fcd9ea3688e02994afb058584573b0e004f70baa
özge baykara sarı <baykaraozge@gmail.com>
2020-10-04 21:36:26 +0300

    Add project files.

---
+++ FrontendTrainingApp/HomePage.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="FrontendTrainingApp.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>HOME</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblFromBackend" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FrontendTrainingApp/HomePage.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp;
+
+namespace FrontendTrainingApp
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var client = new RestClient("http://localhost:54802/api/values/3");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            lblFromBackend.Text = response.Content;
+        }
+    }
+}
